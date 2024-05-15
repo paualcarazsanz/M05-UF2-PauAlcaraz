0ee05a38896c1f8855e2f34e372914db76922470
pau.alcaraz <paualcarazsanz@gmail.com>
2024-05-15 11:31:29 +0200

    CREACIO RULETAVIP.java
    En aquest cas amb aquesta part el codi ja estaria finalitzat, això el que fa es que sigui funcional ja perque tot lo que ha sigut picat abans ja tindria sentit.

---
+++ RuletaVIP.java
@@ -0,0 +1,10 @@
+class RuletaVIP extends Ruleta {
+    public RuletaVIP() {
+        super("VIP", 100);
+    }
+
+    @Override
+    public void metodoAbstracto() {
+        System.out.println("Ruleta VIP.");
+    }
+}
